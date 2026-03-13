ee5755b0ad200b521b00dffe70f1f97c829a097e
M07maaad <marrkk50@gmail.com>
2026-03-13 17:07:20 +0200

    Update AddMedPage.jsx

--- src/pages/AddMedPage.jsx
+++ src/pages/AddMedPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useMedications } from '../hooks/useMedications'
+import { AddMedPage, ProfilePage } from './pages/AddMedPage_ProfilePage'
 import { recognizeDrugFromImage, fileToBase64 } from '../lib/gemini'
 import { Card, Button, Input } from '../components/UI'
 
