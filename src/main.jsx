ffa4b48eb164c36472921e948ecb790238840de5
M07maaad <marrkk50@gmail.com>
2026-03-15 01:05:26 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AuthProvider } from './hooks/useAuth'
+import { MedicationsProvider } from './hooks/useMedications'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <MedicationsProvider>
+          <App />
+        </MedicationsProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
