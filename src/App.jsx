3b4683fac7bba8fcd6a517a00539fcd61ae825f6
M07maaad <marrkk50@gmail.com>
2026-03-15 00:04:49 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import { useAuth } from './hooks/useAuth'
 import LoadingScreen from './components/LoadingScreen'
 import BottomNav from './components/BottomNav'
-
 import {
   OnboardingPage,
   SchedulePage,
@@ -11,9 +10,9 @@ import {
   ProfilePage,
   MedicationsPage,
 } from './pages/Pages'
-import AuthPage     from './pages/AuthPage'
+import AuthPage from './pages/AuthPage'
 import DashboardPage from './pages/DashboardPage'
-import AddMedPage   from './pages/AddMedPage'
+import AddMedPage from './pages/AddMedPage'
 
 function PrivateRoute({ children }) {
   const { user, loading } = useAuth()
@@ -25,21 +24,18 @@ function PrivateRoute({ children }) {
 export default function App() {
   const { user, loading } = useAuth()
   if (loading) return <LoadingScreen />
-
   return (
     <>
       <Routes>
         <Route path="/onboarding" element={<OnboardingPage />} />
         <Route path="/auth" element={user ? <Navigate to="/" replace /> : <AuthPage />} />
-
-        <Route path="/"                element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
-        <Route path="/medications"     element={<PrivateRoute><MedicationsPage /></PrivateRoute>} />
+        <Route path="/" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
+        <Route path="/medications" element={<PrivateRoute><MedicationsPage /></PrivateRoute>} />
         <Route path="/medications/add" element={<PrivateRoute><AddMedPage /></PrivateRoute>} />
-        <Route path="/schedule"        element={<PrivateRoute><SchedulePage /></PrivateRoute>} />
-        <Route path="/interactions"    element={<PrivateRoute><InteractionsPage /></PrivateRoute>} />
-        <Route path="/labs"            element={<PrivateRoute><LabsPage /></PrivateRoute>} />
-        <Route path="/profile"         element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
-
+        <Route path="/schedule" element={<PrivateRoute><SchedulePage /></PrivateRoute>} />
+        <Route path="/interactions" element={<PrivateRoute><InteractionsPage /></PrivateRoute>} />
+        <Route path="/labs" element={<PrivateRoute><LabsPage /></PrivateRoute>} />
+        <Route path="/profile" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
         <Route path="*" element={<Navigate to={user ? '/' : '/onboarding'} replace />} />
       </Routes>
       {user && <BottomNav />}
